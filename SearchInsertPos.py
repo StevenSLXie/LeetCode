e41c665efed2ebe0ddbb3d2c1d8e83957766d5d9
Steven S.L. Xie <stevenslxie@gmail.com>
2014-12-30 21:27:51 +0800

    Update SearchInsertPos.py

--- SearchInsertPos.py
+++ SearchInsertPos.py
@@ -15,3 +15,21 @@ def searchHelper(A,target,left,right):
         return searchHelper(A,target,left,mid)
     else:
         return searchHelper(A,target,mid+1,right)
+        
+        
+def searchInsert(A,target):
+    left = 0
+    right = len(A)-1
+    
+    while left != right:
+        mid = (left+right)//2
+        if target == A[mid]:
+            return mid
+        elif target < A[mid]:
+            right = mid
+        else:
+            left = mid+1
+            
+    if target>A[left]:
+        return left+1
+    return left
